6b7d9c2b6b08f515ed7586815b36c9295e2a7c44
epanteleeva <cat.panteleeva2009@yandex.ru>
2019-05-10 12:29:11 +0400

    Stage all db files

---
+++ src/main/java/com/example/HelloWorld/TestDB.java
@@ -0,0 +1,27 @@
+package com.example.HelloWorld;
+
+import org.hibernate.Session;
+
+
+public class TestDB {
+
+    public void test(){
+/*
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+
+        UsersEntity UsersEntity = new UsersEntity();
+
+        UsersEntity.setEmail("vasyaPupkin@gmail.com");
+        UsersEntity.setPassword("12345");
+        UsersEntity.setDefaultPrivate((byte)1);
+
+        session.save(UsersEntity);
+        session.getTransaction().commit();
+
+        session.close();
+*/
+
+    }
+}
